18f897f7da68e9db48c539a3ab1a076a71411042
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-06-29 21:39:43 +0300

    table edited

--- src/js/Table/Table.js
+++ src/js/Table/Table.js
@@ -1,32 +1,42 @@
 export default class Table {
   constructor(data = null) {
     this.data = data;
-    this.paramData = ['id','title','year', 'imdb'];
+    this.paramData = ['id', 'title', 'year', 'imdb'];
     this.indexParametr = 0;
 
     this.drawTable();
-    this.sortMethod()
+    this.sortMethod();
   }
 
   drawTable() {
     this.table = document.createElement('table');
     this.table.classList.add('table');
-    this.table.innerHTML = `<thead class="thead">
-                              <th>id</th>
-                              <th>title</th>
-                              <th>year</th>
-                              <th>imdb</th>
-                            </thead>  
+    this.table.innerHTML = `<thead class="thead"></thead>  
                             <tbody class="tbody"></tbody>`;
 
-    document.body.appendChild(this.table)
-    this.tbody = document.querySelector('.tbody');
+    document.body.appendChild(this.table);
+    this.drawTHead();
 
     if (this.data == null) {
       return;
     }
-    this.drawTBody();
 
+    this.tbody = document.querySelector('.tbody');
+    this.drawTBody();
+  }
+
+  drawTHead() {
+    this.thead = document.querySelector('.thead');
+
+    const trHead = document.createElement('tr');
+    for (const i of this.paramData) {
+      const th = document.createElement('th');
+      th.classList.add('th', i);
+      th.textContent = i;
+      trHead.appendChild(th);
+    }
+
+    this.thead.appendChild(trHead);
   }
 
   drawTBody() {
@@ -48,18 +58,33 @@ export default class Table {
       this.indexParametr = 0;
     }
     const parametr = this.paramData[this.indexParametr];
-    console.log(parametr);
     setTimeout(() => {
       this.data.sort((a, b) => this.funcSort(a[parametr], b[parametr]));
       this.drawTBody();
+      this.drawArrow(parametr, true);
       setTimeout(() => {
         this.data.reverse();
         this.drawTBody();
+        this.drawArrow(parametr);
         this.indexParametr++;
         this.sortMethod();
       }, 2000);
     }, 2000);
-  
+  }
+
+  drawArrow(column, arrowUp = false) {
+    const arrColumnTitle = document.querySelectorAll('.th');
+    for (const i of arrColumnTitle) {
+      i.classList.remove('increase', 'decrease');
+    }
+
+    const columnElement = document.querySelector(`.${column}`);
+
+    if (arrowUp) {
+      columnElement.classList.add('decrease');
+    } else {
+      columnElement.classList.add('increase');
+    }
   }
 
   funcSort(a, b) {
